cc13a0d642eaf5b1017650f1c6063de26cfc6961
hujikoy <3140102448@zju.edu.cn>
2019-06-02 10:50:55 +0800

    add simple module file

---
+++ simple-lkm.c
@@ -0,0 +1,24 @@
+// Copy from https://www.ibm.com/developerworks/cn/linux/l-proc.html
+
+#include <linux/module.h>
+
+// Define the license for this LKM
+MODULE_LICENSE("GPL");
+
+// Init funtion called on module entry
+int my_module_init(void)
+{
+    printk(KERN_INFO "my_module_init called. Module is now loaded.\n");
+    return 0;
+}
+
+// Cleanup function called on module exit
+void my_module_cleanup(void)
+{
+    printk(KERN_INFO "my_module_cleanup called. Module is now unloaded.\n")
+    return;
+}
+
+// Declear entry and exit functions
+module_init(my_module_init);
+module_exit(my_module_cleanup);
